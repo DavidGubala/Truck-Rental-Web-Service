a58210e1c8cbf53461ddbf37f56d4b969f582957
DavidGubala <gubaladev@gmail.com>
2020-09-19 02:53:09 -0500

    edits to customer license

--- src/com/truck/model/user/Customer.java
+++ src/com/truck/model/user/Customer.java
@@ -9,7 +9,6 @@ import com.truck.model.order.Order;
 public class Customer extends User{
 	private int customerId;
 	private License driverLicense;
-	private int age;
 	private List<Order> orders = new ArrayList<Order>();
 
 	public List<Order> getOrders() {
@@ -18,14 +17,6 @@ public class Customer extends User{
 
 	public void setOrders(List<Order> orders) {
 		this.orders = orders;
-	}
-	
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	public int getAge() {
-		return age; 
 	}
 	
 	public License getDriverLicense() {
