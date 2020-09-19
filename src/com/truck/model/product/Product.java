7e65c5f08bff856579a31e7052efb7f329f43f24
David Gubala <gubdev@protonmail.com>
2020-09-18 20:32:52 -0500

    Edits to models(User, Product, and Order)

---
+++ src/com/truck/model/product/Product.java
@@ -0,0 +1,22 @@
+package com.truck.model.product;
+
+public class Product {
+	private int productId;
+	private double price;
+	
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
---
+++ src/com/truck/model/product/Vehicle.java
@@ -0,0 +1,77 @@
+package com.truck.model.product;
+
+public class Vehicle {
+	private String plateNumber;
+	private String make;
+	private String model;
+	private int year;
+	private int odometer;
+	private String vin;
+	private String type;
+	private boolean available;
+	
+
+	public String getPlateNumber() {
+		return plateNumber;
+	}
+
+	public void setPlateNumber(String plateNumber) {
+		this.plateNumber = plateNumber;
+	}
+	
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public int getOdometer() {
+		return odometer;
+	}
+
+	public void setOdometer(int odometer) {
+		this.odometer = odometer;
+	}
+	
+	public String getVin() {
+		return vin;
+	}
+
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public boolean getAvailability() {
+		return available;
+	}
+	
+	public void setAvailability(boolean available) {
+		this.available = available;
+	}
+}
