066ab570dacfe6eb30d8f07c20639821bb3bf608
hfu2 <hfu2@luc.edu>
2020-09-24 21:42:53 -0500

    ProductManager created, need further construction

--- src/com/truck/dal/ProductDAO.java
+++ src/com/truck/dal/ProductDAO.java
@@ -1,6 +1,15 @@
 package com.truck.dal;
+import com.truck.model.product.Product;
 
 public class ProductDAO {
+	
+	public Product getProduct(String productId) {
+		return null;
+	}
+	
+	public void addProduct(Product product) {
+		return;
+	}
 	/*
 	 * Under Construction
 	 * */
---
+++ src/com/truck/model/service/ProductManager.java
@@ -0,0 +1,31 @@
+package com.truck.model.service;
+
+import com.truck.dal.ProductDAO;
+import com.truck.model.product.Product;
+
+
+public class ProductManager {
+	private ProductDAO prodDAO = new ProductDAO();
+	
+	public Product findProductById(String productId) {
+		
+		try {
+			Product product = prodDAO.getProduct(productId);
+			return product;
+		} catch (Exception se) {
+			System.err.println("ProductManager: Threw a Exception retrieving product.");
+			System.err.println(se.getMessage());
+		}
+		return null;
+	}
+	
+	public void addProduct(Product product) {
+		
+		try {
+			prodDAO.addProduct(product);
+		} catch (Exception se) {
+			System.err.println("ProductManager: Threw a Exception retrieving product.");
+			System.err.println(se.getMessage());
+		}
+	}
+}
