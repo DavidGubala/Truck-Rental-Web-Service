ef6b0b7675104026805c9d6cbbeec4cc0c9b2867
ethomas14 <memma@10.0.0.233>
2020-09-26 03:07:26 -0500

    creating OrderManager

---
+++ src/com/truck/model/service/OrderManager.java
@@ -0,0 +1,43 @@
+package com.truck.model.service;
+
+import com.truck.model.order.*;
+import com.truck.dal.OrderDAO;
+
+
+public class OrderManager {
+	
+	private OrderDAO orderDAO = new OrderDAO();
+
+	// Create
+	public void createOrder() {
+		try {
+			ordervDAO.createOrder();
+	    } catch (Exception se) {
+	      System.err.println("OrderManager: Threw a Exception creating order.");
+	      System.err.println(se.getMessage());
+	    }
+	}
+
+	// Read
+	public Order findOrderById(int orderId) {
+		
+		try {
+			Order order = orderDAO.getOrder(orderId);
+	    	return order;
+	    } catch (Exception se) {
+	      System.err.println("OrderManager: Threw a Exception retrieving Order.");
+	      System.err.println(se.getMessage());
+	    }
+		return null;
+	}
+	
+	public void deleteOrder(int orderId) {
+		try {
+			orderDAO.deleteOrder(orderId);;
+	    } catch (Exception se) {
+	      System.err.println("OrderManager: Threw a Exception deleting order.");
+	      System.err.println(se.getMessage());
+	    }
+	}
+	
+}
